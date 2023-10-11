bb375a8a775ad728994ba6786679f3a3939aa9b2
Özgür Demir <ozguurdemir@gmail.com>
2023-10-11 10:22:10 +0300

    Keyboard sınıfında buyProduct metodu override edildi

--- src/Keyboard.java
+++ src/Keyboard.java
@@ -12,6 +12,20 @@ public class Keyboard extends Product
                 this.type = type;
             }
 
+        @Override
+        public int buyProduct(Product p, Customer c)
+            {
+                if (p.getDescription()=="Corsair")
+                    {
+                        return super.buyProduct(p, c) - 20;
+                    }
+                else
+                    {
+                        return super.buyProduct(p, c);
+                    }
+
+            }
+
         public Keyboard(String id, String description, int price)
             {
                 super(id, description, price);
