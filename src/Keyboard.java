749ce2d7946d995406b88cbc235ddfb1883bee74
Özgür Demir <ozguurdemir@gmail.com>
2023-10-11 10:33:27 +0300

    İndirim değişken ile sağlandı

--- src/Keyboard.java
+++ src/Keyboard.java
@@ -1,23 +1,14 @@
 public class Keyboard extends Product
     {
         private String type;
-
-        public String getType()
-            {
-                return type;
-            }
-
-        public void setType(String type)
-            {
-                this.type = type;
-            }
+        private final int DISCOUNT;
 
         @Override
         public int buyProduct(Product p, Customer c)
             {
                 if (p.getDescription()=="Corsair")
                     {
-                        return super.buyProduct(p, c) + 20;
+                        return super.buyProduct(p, c) + DISCOUNT;
                     }
                 else
                     {
@@ -26,8 +17,18 @@ public class Keyboard extends Product
 
             }
 
-        public Keyboard(String id, String description, int price)
+        public Keyboard(String id, String description, int price,int discount)
             {
                 super(id, description, price);
+                this.DISCOUNT=discount;
+            }
+        public String getType()
+            {
+                return type;
+            }
+
+        public void setType(String type)
+            {
+                this.type = type;
             }
     }
--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,12 @@ public class Main
                 Book book = new Book("1","Roman",60);
                 book.setIsbn("A2S45DW78");
                 book.setPageNumber(390);
-                Keyboard keyboard = new Keyboard("2", "Corsair", 1200);
+                Keyboard keyboard = new Keyboard("2", "Corsair", 1200, 100);
                 keyboard.setType("Mechanical");
                 Mouse mouse = new Mouse("3","Logitech", 900);
                 mouse.setSensor("Optic");
                 int result = book.buyProduct(book, customer1);
                 System.err.println(result);
+                System.err.println(customer1.getId());
             }
     }
